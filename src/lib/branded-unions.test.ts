27cc0db41cf42fa38b9f0718110481a8e111c46f
Joseph T. Lapp <arachnojoe@gmail.com>
2023-05-20 20:45:40 -0400

    test: reorganized branded union tests

--- src/lib/branded-unions.test.ts
+++ src/lib/branded-unions.test.ts
@@ -4,81 +4,90 @@ import { Value } from '@sinclair/typebox/value';
 import { KeyBrandedUnion, ValueBrandedUnion } from './branded-unions';
 
 describe('branded unions', () => {
+  const vSchema = ValueBrandedUnion({
+    brandKey: 'type',
+    schemas: [
+      Type.Object({
+        type: Type.Literal('s'),
+        str1: Type.String(),
+        str2: Type.Optional(Type.String()),
+      }),
+      Type.Object({
+        type: Type.Literal('i'),
+        int1: Type.Integer(),
+        int2: Type.Optional(Type.Integer()),
+      }),
+    ],
+  });
+
+  const kSchema = KeyBrandedUnion({
+    schemas: [
+      Type.Object(
+        {
+          s: Type.String(),
+          str1: Type.String(),
+          str2: Type.Optional(Type.String()),
+        },
+        { uniqueKey: 's' }
+      ),
+      Type.Object(
+        {
+          i: Type.Integer(),
+          int1: Type.Integer(),
+          int2: Type.Optional(Type.Integer()),
+        },
+        { uniqueKey: 'i' }
+      ),
+    ],
+  });
+
   it('ValueBrandedUnion accepts only valid values', () => {
-    const schema = ValueBrandedUnion({
-      brandKey: 'type',
-      schemas: [
-        Type.Object({
-          type: Type.Literal('s'),
-          str: Type.String(),
-        }),
-        Type.Object({
-          type: Type.Literal('i'),
-          int: Type.Integer(),
-        }),
-      ],
-    });
-    expect(Value.Check(schema, { type: 's', str: 'hello' })).toBe(true);
-    expect(Value.Check(schema, { type: 'i', int: 1 })).toBe(true);
+    expect(Value.Check(vSchema, { type: 's', str1: 'hello' })).toBe(true);
+    expect(Value.Check(vSchema, { type: 'i', int1: 1 })).toBe(true);
 
-    expect(Value.Check(schema, { type: 's', str: 'hello', int: 1 })).toBe(true);
-    expect(Value.Check(schema, { type: 'i', str: 'hello', int: 1 })).toBe(true);
+    expect(Value.Check(vSchema, { type: 's', str1: 'hello', int1: 1 })).toBe(
+      true
+    );
+    expect(Value.Check(vSchema, { type: 'i', str1: 'hello', int1: 1 })).toBe(
+      true
+    );
 
-    expect(Value.Check(schema, { type: 's', int: 1 })).toBe(false);
-    expect(Value.Check(schema, { type: 'i', str: 'hello' })).toBe(false);
-    expect(Value.Check(schema, { type: 'x', str: 'hello', int: 1 })).toBe(
+    expect(Value.Check(vSchema, { type: 's', int1: 1 })).toBe(false);
+    expect(Value.Check(vSchema, { type: 'i', str1: 'hello' })).toBe(false);
+    expect(Value.Check(vSchema, { type: 'x', str1: 'hello', int1: 1 })).toBe(
       false
     );
-    expect(Value.Check(schema, { str: 'hello', int: 1 })).toBe(false);
-    expect(Value.Check(schema, {})).toBe(false);
+    expect(Value.Check(vSchema, { str1: 'hello', int1: 1 })).toBe(false);
+    expect(Value.Check(vSchema, {})).toBe(false);
 
-    expect(Value.Check(schema, undefined)).toBe(false);
-    expect(Value.Check(schema, null)).toBe(false);
-    expect(Value.Check(schema, true)).toBe(false);
-    expect(Value.Check(schema, 1)).toBe(false);
-    expect(Value.Check(schema, 'hello')).toBe(false);
+    expect(Value.Check(vSchema, undefined)).toBe(false);
+    expect(Value.Check(vSchema, null)).toBe(false);
+    expect(Value.Check(vSchema, true)).toBe(false);
+    expect(Value.Check(vSchema, 1)).toBe(false);
+    expect(Value.Check(vSchema, 'hello')).toBe(false);
   });
 
   it('KeyBrandedUnion accepts only valid values', () => {
-    const schema = KeyBrandedUnion({
-      schemas: [
-        Type.Object(
-          {
-            s: Type.String(),
-            str: Type.String(),
-          },
-          { uniqueKey: 's' }
-        ),
-        Type.Object(
-          {
-            i: Type.Integer(),
-            int: Type.Integer(),
-          },
-          { uniqueKey: 'i' }
-        ),
-      ],
-    });
+    expect(Value.Check(kSchema, { s: 'hello', str1: 'hello' })).toBe(true);
+    expect(Value.Check(kSchema, { i: 1, int1: 1 })).toBe(true);
 
-    expect(Value.Check(schema, { s: 'hello', str: 'hello' })).toBe(true);
-    expect(Value.Check(schema, { i: 1, int: 1 })).toBe(true);
-
-    expect(Value.Check(schema, { s: 'hello', str: 'hello', int: 1 })).toBe(
+    expect(Value.Check(kSchema, { s: 'hello', str1: 'hello', int1: 1 })).toBe(
       true
     );
-    expect(Value.Check(schema, { i: 1, str: 'hello', int: 1 })).toBe(true);
+    expect(Value.Check(kSchema, { i: 1, str1: 'hello', int1: 1 })).toBe(true);
 
-    expect(Value.Check(schema, { s: 'hello', int: 1 })).toBe(false);
-    expect(Value.Check(schema, { i: 1, str: 'hello' })).toBe(false);
-    expect(Value.Check(schema, { x: 'hello', str: 'hello', int: 1 })).toBe(
+    expect(Value.Check(kSchema, { s: 'hello', int1: 1 })).toBe(false);
+    expect(Value.Check(kSchema, { i: 1, str1: 'hello' })).toBe(false);
+    expect(Value.Check(kSchema, { x: 'hello', str1: 'hello', int1: 1 })).toBe(
       false
     );
-    expect(Value.Check(schema, { str: 'hello', int: 1 })).toBe(false);
-    expect(Value.Check(schema, {})).toBe(false);
+    expect(Value.Check(kSchema, { str1: 'hello', int1: 1 })).toBe(false);
+    expect(Value.Check(kSchema, {})).toBe(false);
 
-    expect(Value.Check(schema, undefined)).toBe(false);
-    expect(Value.Check(schema, null)).toBe(false);
-    expect(Value.Check(schema, true)).toBe(false);
-    expect(Value.Check(schema, 1)).toBe(false);
-    expect(Value.Check(schema, 'hello')).toBe(false);
+    expect(Value.Check(kSchema, undefined)).toBe(false);
+    expect(Value.Check(kSchema, null)).toBe(false);
+    expect(Value.Check(kSchema, true)).toBe(false);
+    expect(Value.Check(kSchema, 1)).toBe(false);
+    expect(Value.Check(kSchema, 'hello')).toBe(false);
   });
 });
