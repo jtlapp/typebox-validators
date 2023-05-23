83b708b49348e23b7721c5e8e1ad3019bcef4e4f
Joseph T. Lapp <arachnojoe@gmail.com>
2023-05-23 15:23:41 -0400

    test: move test wrappers into standard validator test file

--- src/test/standard-validators.test.ts
+++ src/test/standard-validators.test.ts
@@ -1,13 +1,9 @@
-import { TSchema } from '@sinclair/typebox';
+import { TSchema, Type } from '@sinclair/typebox';
 
 import { StandardValidator } from '../validators/standard-validator';
 import { CompilingStandardValidator } from '../validators/compiling-standard-validator';
 import { ValidationException } from '../lib/validation-exception';
-import {
-  SimpleWrapper1,
-  SimpleWrapper2,
-  ValidatorFactory,
-} from './test-wrappers';
+import { AbstractValidator } from '../validators/abstract-validator';
 
 describe('standard validators', () => {
   describe('StandardValidator', () => {
@@ -24,6 +20,84 @@ describe('standard validators', () => {
     testSimpleValidation(validatorFactory);
   });
 });
+
+type ValidatorFactory = <S extends TSchema>(schema: S) => AbstractValidator<S>;
+
+class SimpleWrapper {
+  constructor(readonly validator: AbstractValidator<TSchema>) {}
+}
+
+class SimpleWrapper1 extends SimpleWrapper {
+  delta: number;
+  count: number;
+  name: string;
+
+  static readonly schema = Type.Object({
+    delta: Type.Integer(),
+    count: Type.Integer({ exclusiveMinimum: 0 }),
+    name: Type.RegEx(/[a-zA-Z]+/, {
+      minLength: 5,
+      maxLength: 10,
+      errorMessage: 'name should consist of 5-10 letters',
+    }),
+  });
+
+  constructor(
+    validatorFactory: ValidatorFactory,
+    delta: number,
+    count: number,
+    name: string
+  ) {
+    super(validatorFactory(SimpleWrapper1.schema));
+    this.delta = delta;
+    this.count = count;
+    this.name = name;
+  }
+
+  safeValidate() {
+    this.validator.safeValidate(this, 'Bad SimpleWrapper1');
+  }
+
+  unsafeValidate() {
+    this.validator.unsafeValidate(this, 'Bad SimpleWrapper1');
+  }
+
+  validate(safely: boolean) {
+    this.validator.validate(this, 'Bad SimpleWrapper1', safely);
+  }
+}
+
+class SimpleWrapper2 extends SimpleWrapper {
+  int1: number;
+  int2: number;
+  alpha: string;
+
+  static readonly schema = Type.Object({
+    int1: Type.Integer({ errorMessage: '{field} must be an integer' }),
+    int2: Type.Integer({ errorMessage: '{field} must be an integer' }),
+    alpha: Type.RegEx(/[a-zA-Z]+/, { maxLength: 4 }),
+  });
+
+  constructor(
+    validatorFactory: ValidatorFactory,
+    int1: number,
+    int2: number,
+    alpha: string
+  ) {
+    super(validatorFactory(SimpleWrapper2.schema));
+    this.int1 = int1;
+    this.int2 = int2;
+    this.alpha = alpha;
+  }
+
+  safeValidate() {
+    this.validator.safeValidate(this, 'Bad SimpleWrapper2');
+  }
+
+  unsafeValidate() {
+    this.validator.unsafeValidate(this, 'Bad SimpleWrapper2');
+  }
+}
 
 function testSimpleValidation(validatorFactory: ValidatorFactory) {
   describe('safeValidate()', () => {
