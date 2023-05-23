7dcbd202d833a1092371bc21f1439cbd8da38af2
Joseph T. Lapp <arachnojoe@gmail.com>
2023-05-23 09:44:53 -0400

    feat: export useful validators and error classes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-export * from './lib/lazy-validator';
+export * from './validators/abstract-validator';
+export * from './validators/compiling-discriminated-union-validator';
+export * from './validators/compiling-heterogeneous-union-validator';
+export * from './validators/compiling-standard-validator';
+export * from './validators/discriminated-union-validator';
+export * from './validators/heterogeneous-union-validator';
+export * from './validators/standard-validator';
+
+export * from './lib/union-type-exception';
+export * from './lib/validation-error-detail';
 export * from './lib/validation-exception';
