ef268d74b60a1064cb7a43831ed40965021efa9a
Joseph T. Lapp <arachnojoe@gmail.com>
2023-05-29 16:40:16 -0400

    test: test invalid heterogeneous union validators

--- src/test/heterogeneous-union-validators-invalid.test.ts
+++ src/test/heterogeneous-union-validators-invalid.test.ts
@@ -4,7 +4,10 @@ import { AbstractTypedUnionValidator } from '../validators/abstract-typed-union-
 import { HeterogeneousUnionValidator } from '../validators/heterogeneous-union-validator';
 import { CompilingHeterogeneousUnionValidator } from '../validators/compiling-heterogeneous-union-validator';
 import { ValidationException } from '../lib/validation-exception';
-import { DEFAULT_OVERALL_ERROR } from '../lib/errors';
+import {
+  DEFAULT_OVERALL_ERROR,
+  DEFAULT_UNKNOWN_TYPE_MESSAGE,
+} from '../lib/errors';
 import {
   ValidatorKind,
   MethodKind,
@@ -19,14 +22,18 @@ const onlyRunMethod = MethodKind.All;
 
 const wellFormedUnion1 = Type.Union([
   Type.Object({
-    s: Type.String(),
+    unique1: Type.String(),
     str1: Type.String(),
     str2: Type.Optional(Type.String()),
   }),
   Type.Object({
-    i: Type.Integer(),
+    unique2: Type.Integer(),
     int1: Type.Integer(),
-    int2: Type.Optional(Type.Integer()),
+    int2: Type.Optional(
+      Type.Integer({
+        errorMessage: "'{field}' must be an integer",
+      })
+    ),
   }),
 ]);
 
@@ -35,30 +42,19 @@ const wellFormedUnion2 = Type.Union(
     Type.Object({
       str1: Type.String(),
       str2: Type.String(),
-      s1: Type.String(),
-      unique: Type.String(),
-      extra: Type.Optional(Type.String()),
+      unique1: Type.String(),
+      unique3: Type.String(),
+      opt: Type.Optional(Type.String()),
     }),
     Type.Object({
       str1: Type.String(),
-      s2: Type.String(),
+      unique2: Type.String(),
       str2: Type.String(),
-      extra: Type.Optional(Type.Integer()),
+      opt: Type.Optional(Type.Integer()),
     }),
   ],
-  { typeError: 'Unknown type' }
+  { errorMessage: 'Unknown type' }
 );
-
-const illFormedUnion = Type.Union([
-  Type.Object({
-    s: Type.String(),
-    str1: Type.Optional(Type.String()),
-  }),
-  Type.Object({
-    s: Type.Integer(),
-    int1: Type.Optional(Type.Integer()),
-  }),
-]);
 
 const validatorCache = new ValidatorCache();
 
@@ -76,122 +72,165 @@ describe('heterogenous union validators - invalid values', () => {
   }
   if (runThisValidator(ValidatorKind.Compiling)) {
     describe('CompilingHeterogeneousUnionValidator', () => {
-      testValidator((schema: TSchema) =>
-        validatorCache.getCompiling(
-          schema,
-          () =>
-            new CompilingHeterogeneousUnionValidator(
-              schema as TUnion<TObject[]>
-            )
-        )
+      testValidator(
+        (schema: TSchema) =>
+          validatorCache.getCompiling(
+            schema,
+            () =>
+              new CompilingHeterogeneousUnionValidator(
+                schema as TUnion<TObject[]>
+              )
+          ) as AbstractTypedUnionValidator<TUnion<TObject[]>>
       );
     });
   }
 });
 
 function testValidator(
-  createValidator: (schema: TSchema) => AbstractStandardValidator<TSchema>
+  createValidator: (
+    schema: TSchema
+  ) => AbstractTypedUnionValidator<TUnion<TObject[]>>
 ) {
+  const defaultString = `${DEFAULT_OVERALL_ERROR}:\n- ${DEFAULT_UNKNOWN_TYPE_MESSAGE}`;
   testInvalidSpecs([
     {
-      description: 'single invalid field with one error',
+      description: 'selects 1st union member, 1st key unique, single error',
       onlySpec: false,
-      schema: schema1,
-      value: { delta: 0.5, count: 1, name: 'ABCDE' },
+      schema: wellFormedUnion1,
+      value: { unique1: 'hello', int1: 1 },
       assertMessage: DEFAULT_OVERALL_ERROR,
-      errors: [{ path: '/delta', message: 'Expected integer' }],
-      assertString: 'Invalid value:\n- delta: Expected integer',
-      validateString: 'Invalid value:\n- delta: Expected integer',
+      errors: [{ path: '/str1', message: 'Expected string' }],
+      assertString: 'Invalid value:\n- str1: Expected string',
+      validateString: 'Invalid value:\n- str1: Expected string',
+    },
+    {
+      description: 'selects 2nd union member, 1st key unique, multiple errors',
+      onlySpec: false,
+      schema: wellFormedUnion1,
+      value: { unique2: 1, str1: 'hello', int2: 'hello' },
+      assertMessage: DEFAULT_OVERALL_ERROR,
+      errors: [
+        { path: '/int1', message: 'Expected integer' },
+        { path: '/int2', message: "'int2' must be an integer" },
+      ],
+      assertString: 'Invalid value:\n- int1: Expected integer',
+      validateString:
+        "Invalid value:\n- int1: Expected integer\n- int2: 'int2' must be an integer",
+    },
+    {
+      description: 'unique field not selecting any union member',
+      onlySpec: false,
+      schema: wellFormedUnion1,
+      value: { x: 'hello', str1: 'hello', int1: 1 },
+      assertMessage: DEFAULT_OVERALL_ERROR,
+      errors: [{ path: '', message: DEFAULT_UNKNOWN_TYPE_MESSAGE }],
+      assertString: defaultString,
+      validateString: defaultString,
+    },
+    {
+      description: 'unique fields selecting multiple union members',
+      onlySpec: false,
+      schema: wellFormedUnion1,
+      value: { str1: 'hello', int1: 1 },
+      assertMessage: DEFAULT_OVERALL_ERROR,
+      errors: [{ path: '', message: DEFAULT_UNKNOWN_TYPE_MESSAGE }],
+      assertString: defaultString,
+      validateString: defaultString,
+    },
+    {
+      description: 'empty object value',
+      onlySpec: false,
+      schema: wellFormedUnion1,
+      value: {},
+      assertMessage: DEFAULT_OVERALL_ERROR,
+      errors: [{ path: '', message: DEFAULT_UNKNOWN_TYPE_MESSAGE }],
+      assertString: defaultString,
+      validateString: defaultString,
+    },
+    {
+      description: 'undefined value',
+      onlySpec: false,
+      schema: wellFormedUnion1,
+      value: undefined,
+      assertMessage: DEFAULT_OVERALL_ERROR,
+      errors: [{ path: '', message: DEFAULT_UNKNOWN_TYPE_MESSAGE }],
+      assertString: defaultString,
+      validateString: defaultString,
+    },
+    {
+      description: 'null value',
+      onlySpec: false,
+      schema: wellFormedUnion1,
+      value: null,
+      assertMessage: DEFAULT_OVERALL_ERROR,
+      errors: [{ path: '', message: DEFAULT_UNKNOWN_TYPE_MESSAGE }],
+      assertString: defaultString,
+      validateString: defaultString,
+    },
+    {
+      description: 'simple literal value',
+      onlySpec: false,
+      schema: wellFormedUnion1,
+      value: 'hello',
+      assertMessage: DEFAULT_OVERALL_ERROR,
+      errors: [{ path: '', message: DEFAULT_UNKNOWN_TYPE_MESSAGE }],
+      assertString: defaultString,
+      validateString: defaultString,
     },
     {
       description:
-        'single invalid field with one error, custom overall message 1',
+        'selects 1st union member, non-1st key unique, invalid, with overall message',
       onlySpec: false,
-      schema: schema1,
-      value: { delta: 0.5, count: 1, name: 'ABCDE' },
+      schema: wellFormedUnion2,
+      value: {
+        str1: 'a',
+        str2: 'b',
+        unique1: 'c',
+        unique3: 'd',
+        opt: 32,
+      },
       overallMessage: 'Custom message',
       assertMessage: 'Custom message',
-      errors: [{ path: '/delta', message: 'Expected integer' }],
-      assertString: 'Custom message:\n- delta: Expected integer',
-      validateString: 'Custom message:\n- delta: Expected integer',
+      errors: [{ path: '/opt', message: 'Expected string' }],
+      assertString: 'Custom message:\n- opt: Expected string',
+      validateString: 'Custom message:\n- opt: Expected string',
     },
     {
       description:
-        'single invalid field with one error, custom overall message 2',
+        'selects 2nd union member, non-1st key unique, invalid key value',
       onlySpec: false,
-      schema: schema1,
-      value: { delta: 0.5, count: 1, name: 'ABCDE' },
-      overallMessage: "Oopsie. '{field}' {detail}",
-      assertMessage: "Oopsie. 'delta' Expected integer",
-      errors: [{ path: '/delta', message: 'Expected integer' }],
-      assertString:
-        "Oopsie. 'delta' Expected integer:\n- delta: Expected integer",
-      validateString: "Oopsie. '{field}' {detail}:\n- delta: Expected integer",
-    },
-    {
-      description: 'single invalid field with multiple errors',
-      onlySpec: false,
-      schema: schema2,
-      value: { int1: 1, int2: 1, alpha: '12345' },
-      errors: [
-        {
-          path: '/alpha',
-          message: 'less or equal to 4',
-        },
-        {
-          path: '/alpha',
-          message: 'match pattern',
-        },
-      ],
-    },
-    {
-      description: 'multiple invalid fields with multiple errors',
-      onlySpec: false,
-      schema: schema2,
-      value: { int1: 1.5, int2: 1.5, alpha: '12345' },
+      schema: wellFormedUnion2,
+      value: { str1: 'a', unique2: 1, str2: 'c', opt: 32 },
       assertMessage: DEFAULT_OVERALL_ERROR,
-      errors: [
-        {
-          path: '/int1',
-          message: 'int1 must be an integer',
-        },
-        {
-          path: '/int2',
-          message: 'int2 must be an integer',
-        },
-        {
-          path: '/alpha',
-          message: 'less or equal to 4',
-        },
-        {
-          path: '/alpha',
-          message: 'match pattern',
-        },
-      ],
+      errors: [{ path: '/unique2', message: 'Expected string' }],
+      assertString: 'Invalid value:\n- unique2: Expected string',
+      validateString: 'Invalid value:\n- unique2: Expected string',
     },
     {
-      description: 'one custom error message for multiple errors',
+      description: 'not selecting any union member, with custom type error',
       onlySpec: false,
-      schema: schema1,
-      value: { delta: 0.5, count: 1, name: '1' },
+      schema: wellFormedUnion2,
+      value: { x: 'not-there' },
       assertMessage: DEFAULT_OVERALL_ERROR,
-      errors: [
-        {
-          path: '/delta',
-          message: 'integer',
-        },
-        {
-          path: '/name',
-          message: 'name should consist of 5-10 letters',
-        },
-      ],
-      assertString: 'Invalid value:\n- delta: Expected integer',
-      validateString:
-        'Invalid value:\n- delta: Expected integer\n- name: name should consist of 5-10 letters',
+      errors: [{ path: '', message: 'Unknown type' }],
+      assertString: 'Invalid value:\n- Unknown type',
+      validateString: 'Invalid value:\n- Unknown type',
     },
+    // {
+    //   description:
+    //     'not selecting any union member, with custom overall and type errors',
+    //   onlySpec: false,
+    //   schema: wellFormedUnion2,
+    //   value: { opt: 32 },
+    //   overallMessage: "Oopsie. '{field}' {detail}",
+    //   assertMessage: "Oopsie. 'Value' Unknown type",
+    //   errors: [{ path: '', message: 'Unknown type' }],
+    //   assertString: "Oopsie. 'Value' Unknown type:\n- Unknown type",
+    //   validateString: "Oopsie. '{field}' {detail}:\n- Unknown type",
+    // },
   ]);
 
-  function testInvalidSpecs(specs: InvalidTestSpec<TSchema>[]) {
+  function testInvalidSpecs(specs: InvalidTestSpec<TUnion<TObject[]>>[]) {
     if (runThisTest(MethodKind.Test)) {
       describe('test() rejections', () => {
         specsToRun(specs).forEach((spec) => {
@@ -239,8 +278,8 @@ function testValidator(
     }
   }
 
-  function testAssertMethodRejection<S extends TSchema>(
-    method: ValidatorMethodOfClass<StandardValidator<S>>,
+  function testAssertMethodRejection<S extends TUnion<TObject[]>>(
+    method: ValidatorMethodOfClass<AbstractTypedUnionValidator<S>>,
     specs: InvalidTestSpec<TSchema>[]
   ) {
     describe(`${method}() rejections`, () => {
@@ -249,7 +288,7 @@ function testValidator(
           const validator = createValidator(spec.schema);
           try {
             (validator[method] as any)(spec.value, spec.overallMessage);
-            fail('should have thrown');
+            expect(false).toBe(true);
           } catch (e: any) {
             if (!(e instanceof ValidationException)) throw e;
 
@@ -271,8 +310,8 @@ function testValidator(
     });
   }
 
-  function testValidateMethodRejection<S extends TSchema>(
-    method: ValidatorMethodOfClass<StandardValidator<S>>,
+  function testValidateMethodRejection<S extends TUnion<TObject[]>>(
+    method: ValidatorMethodOfClass<AbstractTypedUnionValidator<S>>,
     specs: InvalidTestSpec<TSchema>[]
   ) {
     describe(`${method}() rejections`, () => {
@@ -281,7 +320,7 @@ function testValidator(
           const validator = createValidator(spec.schema);
           try {
             (validator[method] as any)(spec.value, spec.overallMessage);
-            fail('should have thrown');
+            expect(false).toBe(true);
           } catch (e: any) {
             if (!(e instanceof ValidationException)) throw e;
 
