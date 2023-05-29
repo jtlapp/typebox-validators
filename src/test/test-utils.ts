21ea3ae1ee286f1f590f7bba01f136eab4cb99cc
Joseph T. Lapp <arachnojoe@gmail.com>
2023-05-29 16:33:12 -0400

    test: remove no-longer used test utils

--- src/test/test-utils.ts
+++ src/test/test-utils.ts
@@ -1,8 +1,6 @@
 import { TObject, TSchema, TUnion } from '@sinclair/typebox';
 import { AbstractValidator } from '../validators/abstract-validator';
 
-export const OVERALL_MESSAGE = 'Invalid union value';
-
 export enum ValidatorKind {
   All,
   Compiling,
@@ -98,44 +96,5 @@ export class ValidatorCache {
       cache.set(schema, validator);
     }
     return validator;
-  }
-}
-
-// TODO: still using this?
-export function checkValidations(
-  validator: AbstractValidator<TUnion<TObject[]>>,
-  schemaIndex: number,
-  value: any,
-  valid: boolean
-): void {
-  const expectedSchema = validator.schema.anyOf[schemaIndex];
-
-  tryValidation(valid, expectedSchema, 'Invalid value', () =>
-    validator.assert(value)
-  );
-  tryValidation(valid, expectedSchema, OVERALL_MESSAGE, () =>
-    validator.assert(value, OVERALL_MESSAGE)
-  );
-
-  tryValidation(valid, expectedSchema, 'Invalid value', () =>
-    validator.validate(value)
-  );
-  tryValidation(valid, expectedSchema, OVERALL_MESSAGE, () =>
-    validator.validate(value, OVERALL_MESSAGE)
-  );
-}
-
-// TODO: still using this?
-function tryValidation(
-  valid: boolean,
-  expectedSchema: TSchema,
-  expectedMessage: string,
-  validate: () => void
-): void {
-  if (valid) {
-    const actualSchema = validate();
-    expect(actualSchema).toBe(expectedSchema);
-  } else {
-    expect(validate).toThrow(expectedMessage);
   }
 }
