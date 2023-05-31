80ef3fa5b248b2ef4e70c2216a3c1fb2919a9bf2
Joseph T. Lapp <arachnojoe@gmail.com>
2023-05-30 20:54:03 -0400

    fix: error when union member lacks discriminant key

--- src/validators/compiling-discriminated-union-validator.ts
+++ src/validators/compiling-discriminated-union-validator.ts
@@ -90,8 +90,14 @@ export class CompilingDiscriminatedUnionValidator<
           switch (${this.toValueKeyDereference(this.discriminantKey)}) {\n`,
       ];
       for (let i = 0; i < this.schema.anyOf.length; ++i) {
-        const literal =
-          this.schema.anyOf[i].properties[this.discriminantKey].const;
+        const discriminantSchema =
+          this.schema.anyOf[i].properties[this.discriminantKey];
+        if (discriminantSchema === undefined) {
+          throw Error(
+            `Discriminant key '${this.discriminantKey}' not present in all members of discriminated union`
+          );
+        }
+        const literal = discriminantSchema.const;
         if (typeof literal === 'string') {
           codeParts.push(
             `case '${literal.replace(/'/g, "\\'")}': return ${i};\n`
