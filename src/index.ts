4bfe769018442430934b7138f636f13c42550369
Joseph T. Lapp <arachnojoe@gmail.com>
2023-06-21 22:40:28 -0400

    chore: temporarily revert all exports to root index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export * from './validators/abstract/abstract-validator';
+export * from './validators/standard/index';
+export * from './validators/heterogeneous/index';
+export * from './validators/discriminated/index';
 export * from './lib/validation-exception';
 
 export { ValueError } from '@sinclair/typebox/value';
